cd137bc9b0ea318639baa2b1ed28c52e05f2a2bc
Crantemax <worthyz.enterprise@gmail.com>
2026-06-01 16:32:09 +0200

    Add TransparencyBanner component to LogicielPageClient

--- app/logiciels/[slug]/LogicielPageClient.tsx
+++ app/logiciels/[slug]/LogicielPageClient.tsx
@@ -100,6 +100,7 @@ export function LogicielPageClient({ logicielSlug }: LogicielPageClientProps) {
       </div>
 
       <AffiliateDisclosure />
+      <TransparencyBanner />
 
       <main className="max-w-4xl mx-auto px-6 lg:px-8 py-16">
         
